74a8282923da0606ba4d2e9cd62eec74a0b427d2
Elmer E. Alanoca <elmer.jage21@gmail.com>
2022-08-13 23:13:10 -0400

    7

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.write('''# **10 Cool Beginner Python Tricks That Will Make Your Life Easier**
 
-Simple but effective tips for every python lovers''')
+##Simple but effective tips for every python lovers''')
 
 from PIL import Image
 image = Image.open('1_5IFgojJ4nU8f0YKTcjWDrg.jpeg')
