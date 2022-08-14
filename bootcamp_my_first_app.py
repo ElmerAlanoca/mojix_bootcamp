01c28635abf8e1e2754b937414aa5d5a4315f2c3
Elmer E. Alanoca <elmer.jage21@gmail.com>
2022-08-14 02:22:10 -0400

    23

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -25,7 +25,7 @@ st.write('''The `Walrus` **or** `:=` operator is one of the latest additions to 
 It is an assignment operator that lets you assign value to a variable within an expression 
 like conditional statements, loops, etc.''')
 
-st.write('''__|__ *** Example***
+st.write('''__| _Example_ __
 
 If we want to check and print the length of a list:''')
 code = '''
@@ -34,7 +34,7 @@ if(l := len(mylist) > 2)
 print(l)'''
 st.code(code, language='python')
 
-st.write('''__|__ *** Output***''')
+st.write('''__| _ Output_ __''')
 code = '''
 3'''
 st.code(code, language='python')
