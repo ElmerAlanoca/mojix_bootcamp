50d204e9430b864169c6a407876dfa7d9d2c57a0
Elmer E. Alanoca <elmer.jage21@gmail.com>
2022-08-13 23:10:37 -0400

    6

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -3,10 +3,17 @@ import streamlit as st
 st.write('''# **10 Cool Beginner Python Tricks That Will Make Your Life Easier**
 
 Simple but effective tips for every python lovers''')
-#st.write("Simple but effective tips for every python lovers")
 
 from PIL import Image
 image = Image.open('1_5IFgojJ4nU8f0YKTcjWDrg.jpeg')
 
 st.image(image, caption='Photo by Miesha Maiden from Pexels')
+
+st.write('''The compactness of Python can make a developer’s life a lot easier when 
+writing lines and lines of code. But there are some lesser-known Python tricks that 
+can surprise you with their amazing capabilities.
+
+In today’s article, I will discuss 10 Python tips and tricks that will be really 
+helpful for beginners to write more compact code. Knowing these tips and tricks 
+will definitely save you some valuable time.''')
 
